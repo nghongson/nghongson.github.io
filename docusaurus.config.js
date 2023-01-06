22cb42f469a847fbb054a4db3b603868d8998503
LeoNguyen <leonguyen@pop-os.localdomain>
2023-01-06 15:01:48 +0700

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,7 +89,7 @@ const config = {
       },
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: false,
+        disableSwitch: true,
         respectPrefersColorScheme: false,
       },
     }),
