467358af3871e0ec32ae90129cc37e5f6b60142e
LeoNguyen <leonguyen@pop-os.localdomain>
2023-01-06 13:22:56 +0700

    update tailwind

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,6 +27,20 @@ const config = {
     locales: ['en'],
   },
 
+  plugins: [
+    async function tailwindPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
+
   presets: [
     [
       'classic',
